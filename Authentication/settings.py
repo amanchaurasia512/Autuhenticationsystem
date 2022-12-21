32618eed3e271aeee1de9fcf6b51c8490168549a
dev ops <devops@agile.com.np>
2022-12-21 17:43:38 +0545

    fourthcommit

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -135,6 +135,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aman.1si17cs008@gmail.com'
-EMAIL_HOST_PASSWORD = 'rpeicjmknwcpzqka'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 
